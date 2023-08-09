3643d24d0e4e805ef0cbb60faaec4da3d4adb53c
Mariia Zaripova <zaripova.mary.m@gmail.com>
2023-08-09 20:59:22 +0300

    add decorators

---
+++ Python_advanced/decorators/logger_params.py
@@ -0,0 +1,62 @@
+import os
+from datetime import datetime
+from functools import wraps
+
+
+def logger(path):
+    
+    def __logger(old_function):
+        @wraps(old_function)
+        def new_function(*args, **kwargs):
+            result = old_function(*args, **kwargs)
+            with open(path, 'a') as file:
+                file.write(f'{datetime.today().strftime("%d.%m.%Y %H:%M")} func_name: {old_function.__name__} args: {args}, kwargs: {kwargs} result: {result}\n')
+            return result
+
+        return new_function
+
+    return __logger
+
+
+def test_2():
+    paths = ('log_1.log', 'log_2.log', 'log_3.log')
+
+    for path in paths:
+        if os.path.exists(path):
+            os.remove(path)
+
+        @logger(path)
+        def hello_world():
+            return 'Hello World'
+
+        @logger(path)
+        def summator(a, b=0):
+            return a + b
+
+        @logger(path)
+        def div(a, b):
+            return a / b
+
+        assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+        result = summator(2, 2)
+        assert isinstance(result, int), 'Должно вернуться целое число'
+        assert result == 4, '2 + 2 = 4'
+        result = div(6, 2)
+        assert result == 3, '6 / 2 = 3'
+        summator(4.3, b=2.2)
+
+    for path in paths:
+
+        assert os.path.exists(path), f'файл {path} должен существовать'
+
+        with open(path) as log_file:
+            log_file_content = log_file.read()
+
+        assert 'summator' in log_file_content, 'должно записаться имя функции'
+
+        for item in (4.3, 2.2, 6.5):
+            assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_2()
